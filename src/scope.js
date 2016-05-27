8a679e314bacef057cc4d2701eceff73e4cac731
visviva <vis.viva@live.com>
2016-05-27 17:13:59 +0400

    $emit and $brodacast implemented

--- src/scope.js
+++ src/scope.js
@@ -247,6 +247,7 @@ Scope.prototype.$$everyScope = function(fn) {
 };
 
 Scope.prototype.$destroy = function() {
+  this.$broadcast('$destroy');
   if (this.$parent) {
     var siblings = this.$parent.$$children;
     var indexOfThis = siblings.indexOf(this);
@@ -255,6 +256,7 @@ Scope.prototype.$destroy = function() {
     }
   }
   this.$$watchers = [];
+  this.$$listeners = {};
 };
 
 Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
@@ -345,35 +347,65 @@ Scope.prototype.$on = function(eventName, listener) {
   }
   listeners.push(listener);
   return function() {
-var index = listeners.indexOf(listener);
-if (index >= 0) {
-listeners[index] = null;
-}
-};
+    var index = listeners.indexOf(listener);
+    if (index >= 0) {
+      listeners[index] = null;
+    }
+  };
 };
 
 Scope.prototype.$emit = function(eventName) {
-  var additionalArgs = _.drop(arguments);
-  return this.$$fireEventOnScope(eventName, additionalArgs);
+  var propagationStopped = false;
+  var event = {name: eventName,
+    targetScope: this,
+    stopPropagation: function() {
+      propagationStopped = true;
+    },
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  var listenerArgs = [event].concat(_.drop(arguments));
+  var scope = this;
+  do {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    scope = scope.$parent;
+  } while (scope && !propagationStopped);
+  event.currentScope = null;
+  return event;
 };
 
 Scope.prototype.$broadcast = function(eventName) {
-  var additionalArgs = _.drop(arguments);
-  return this.$$fireEventOnScope(eventName, additionalArgs);
+  var event = {name: eventName,
+    targetScope: this,
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  var listenerArgs = [event].concat(_.drop(arguments));
+  this.$$everyScope(function(scope) {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    return true;
+  });
+  event.currentScope = null;
+  return event;
 };
 
-Scope.prototype.$$fireEventOnScope = function(eventName, additionalArgs) {
-  var event = {name: eventName};
-  var listenerArgs = [event].concat(additionalArgs);
+Scope.prototype.$$fireEventOnScope = function(eventName, listenerArgs) {
   var listeners = this.$$listeners[eventName] || [];
   var i = 0;
   while (i < listeners.length) {
     if (listeners[i] === null) {
       listeners.splice(i, 1);
     } else {
-      listeners[i].apply(null, listenerArgs);
+      try {
+        listeners[i].apply(null, listenerArgs);
+      } catch (e) {
+        console.error(e);
+      }
       i++;
     }
   }
-  return event;
-};
+};
