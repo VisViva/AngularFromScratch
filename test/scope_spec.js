8e06dcba11c5f57abd1a602de5be54eda85977d2
VisViva <a.vis.viva@gmail.com>
2016-05-29 17:09:31 +0500

    Parsing numbers

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,5 +1,9 @@
+/* jshint globalstrict: true */
+/* global Scope: false */
+
+'use strict';
+
 describe("Scope", function(){
-  'use strict';
   it("can be constructed and used as an object", function(){
     var scope = new Scope();
     scope.aProperty = 1;
